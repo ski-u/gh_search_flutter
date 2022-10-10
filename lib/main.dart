9f1cc7c314ac3a1adde6cdc9052a3ee248548b8f
Yunosuke Sakai <qmkns246@gmail.com>
2022-10-10 13:56:16 +0900

    Add search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:gh_search_flutter/model/repo.dart';
@@ -32,10 +33,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Timer? _debounce;
+  final _queryController = TextEditingController();
   var _repos = <Repo>[];
 
-  Future<void> _fetchRepos() async {
-    const url = 'https://api.github.com/search/repositories?q=flutter';
+  void _onQueryChanged(String query) {
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(milliseconds: 300), () {
+      _fetchRepos(query);
+    });
+  }
+
+  Future<void> _fetchRepos(String query) async {
+    final url = 'https://api.github.com/search/repositories?q=$query';
     final response = await http.get(Uri.parse(url));
 
     setState(() {
@@ -48,7 +58,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    _fetchRepos();
   }
 
   @override
@@ -57,67 +66,90 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemCount: _repos.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 12,
-                      backgroundImage: NetworkImage(
-                        _repos[index].owner.avatarUrl,
-                      ),
-                      backgroundColor: Colors.transparent,
-                    ),
-                    const SizedBox(width: 4),
-                    Text(
-                      _repos[index].owner.login,
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  _repos[index].name,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  _repos[index].description ?? '',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  children: [
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.star_border_outlined,
-                          color: Colors.grey,
-                        ),
-                        Text(
-                          '${_repos[index].stargazersCount}',
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(width: 16),
-                    Text(
-                      _repos[index].language ?? '',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  ],
-                ),
-              ],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
             ),
-          );
-        },
+            child: TextField(
+              controller: _queryController,
+              decoration: const InputDecoration(
+                labelText: 'Search',
+                suffixIcon: Icon(Icons.search),
+              ),
+              onChanged: _onQueryChanged,
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(16),
+              itemCount: _repos.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 8,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 12,
+                            backgroundImage: NetworkImage(
+                              _repos[index].owner.avatarUrl,
+                            ),
+                            backgroundColor: Colors.transparent,
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            _repos[index].owner.login,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        _repos[index].name,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        _repos[index].description ?? '',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.star_border_outlined,
+                                color: Colors.grey,
+                              ),
+                              Text(
+                                '${_repos[index].stargazersCount}',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 16),
+                          Text(
+                            _repos[index].language ?? '',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
